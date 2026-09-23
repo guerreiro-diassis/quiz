502f0c541948e2835c730eabaaf95368c92d98c3
Diassis Guerreiro <guerreiro.diassis@gmail.com>
2026-09-23 20:46:10 -0300

    primeiro commit

---
+++ scripts/quiz.js
@@ -0,0 +1,6 @@
+const question1 = document.getElementById('question-1');
+
+console.log('questão 1: ' + question1)
+console.log('classes de CSS da questão 1: ' + question1.classList)
+
+question1.classList.remove('quiz__question--hidden')
