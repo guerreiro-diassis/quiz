c6fe6455dc93286fd21ce8377fe79f8b1f1d560a
Diassis Guerreiro <guerreiro.diassis@gmail.com>
2026-09-23 21:51:59 -0300

    alinhamento codigo professor

--- scripts/quiz.js
+++ scripts/quiz.js
@@ -4,3 +4,5 @@ console.log('questão 1: ' + question1)
 console.log('classes de CSS da questão 1: ' + question1.classList)
 
 question1.classList.remove('quiz__question--hidden')
+
+console.log('questão 1: classes css', question1.classList)
