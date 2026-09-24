3c8d789a3e2dbafb6197238925edbbed568dcfd2
Diassis Guerreiro <guerreiro.diassis@gmail.com>
2026-09-23 22:46:20 -0300

    retorno a minha versao

--- scripts/quiz.js
+++ scripts/quiz.js
@@ -1,8 +1,4 @@
-const question1 = document.getElementById('question-1');
+const question1 = document.getElementById("question-1");
 
-console.log('questão 1: ' + question1)
-console.log('classes de CSS da questão 1: ' + question1.classList)
-
-question1.classList.remove('quiz__question--hidden')
-
+console.log('questão 1', question1)
 console.log('questão 1: classes css', question1.classList)
