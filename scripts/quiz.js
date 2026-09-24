bbce66b7675cc0092e0eb75c1fb0a252be93b367
Diassis Guerreiro <guerreiro.diassis@gmail.com>
2026-09-24 00:04:36 -0300

    adiciona interacao com botoes e novos displays

--- scripts/quiz.js
+++ scripts/quiz.js
@@ -1,4 +1,52 @@
-const question1 = document.getElementById("question-1");
+const startQuizButton = document.querySelector('#start-quiz');
+const finishQuizButton = document.querySelector('#finish-quiz');
 
-console.log('questão 1', question1)
-console.log('questão 1: classes css', question1.classList)
+const question1 = document.querySelector('#question1');
+const question2 = document.querySelector('#question2');
+const question3 = document.querySelector('#question3');
+
+startQuizButton.addEventListener('click', () => {
+// escuta botão e o esconde
+  startQuizButton.classList.add('quiz__btn--hidden')
+
+// escuta botão e faz com que pergunta apareça  
+  question1.classList.remove('quiz__question--hidden') 
+});
+
+// pega primeira pergunta e escuta evento de submit
+question1.addEventListener('submit', (event) => {
+// previne comportamento padrão
+    event.preventDefault()
+
+// para desabilitar todas as opções de resposta
+    question1.querySelectorAll('.quiz__option').forEach(answer => {
+        answer.setAttribute('disabled', true)
+    });
+
+// faz com que a próxima pergunta apareça
+    question2.classList.remove('quiz__question--hidden')
+})
+
+question2.addEventListener('submit', (event) => {
+    event.preventDefault()
+    
+    question2.querySelectorAll('.quiz__option').forEach(answer => {
+        answer.setAttribute('disabled', true)
+    });
+    
+    question3.classList.remove('quiz__question--hidden')
+});
+
+question3.addEventListener('submit', (event) => {
+    event.preventDefault()
+    
+    question3.querySelectorAll('.quiz__option').forEach(answer => {
+        answer.setAttribute('disabled', true)
+    });
+    
+    finishQuizButton.classList.remove('quiz__btn--hidden')
+});
+
+finishQuizButton.addEventListener('click', () => {
+
+})
